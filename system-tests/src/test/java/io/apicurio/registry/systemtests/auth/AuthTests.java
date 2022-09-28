ff3921416988c813e94abc2a41bc97b17d49bbef
Ondrej Babec <ond.babec@gmail.com>
2022-09-28 13:03:40 +0200

    Add profile

--- system-tests/src/test/java/io/apicurio/registry/systemtests/auth/AuthTests.java
+++ system-tests/src/test/java/io/apicurio/registry/systemtests/auth/AuthTests.java
@@ -16,12 +16,14 @@ import io.apicurio.registry.systemtests.framework.Constants;
 import io.apicurio.registry.systemtests.registryinfra.resources.KafkaKind;
 import io.apicurio.registry.systemtests.registryinfra.resources.PersistenceKind;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Tag("auth")
 public abstract class AuthTests extends TestBase {
     /* TEST RUNNERS */
 
