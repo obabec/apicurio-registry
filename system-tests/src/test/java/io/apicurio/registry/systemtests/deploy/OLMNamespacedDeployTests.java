7db8a96c1c48b74cf29ee33c859295a48985e20d
Radim Kubis <rkubis@redhat.com>
2022-11-29 10:10:15 +0100

    New(tests): Tags and profiles for test categories

--- system-tests/src/test/java/io/apicurio/registry/systemtests/deploy/OLMNamespacedDeployTests.java
+++ system-tests/src/test/java/io/apicurio/registry/systemtests/deploy/OLMNamespacedDeployTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Tag;
 
 @Tag("olm")
 @Tag("olm-namespace")
+@Tag("olm-namespace-deploy")
 public class OLMNamespacedDeployTests extends OLMDeployTests {
     @Override
     public void setupTestClass() {
