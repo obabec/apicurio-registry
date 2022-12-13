434540c725ec9b354ac67fe3e35c3d84a7ddb9d1
Radim Kubis <rkubis@redhat.com>
2022-12-13 08:41:18 +0100

    Update(DeployTests): Set bundle path for tests

--- system-tests/src/test/java/io/apicurio/registry/systemtests/deploy/BundleDeployTests.java
+++ system-tests/src/test/java/io/apicurio/registry/systemtests/deploy/BundleDeployTests.java
@@ -18,8 +18,10 @@ public class BundleDeployTests extends DeployTests {
     public void testBeforeEach(ExtensionContext testContext) throws InterruptedException {
         LOGGER.info("BeforeEach: " + testContext.getTestMethod().get().getName());
 
-        ApicurioRegistryBundleOperatorType registryBundleOperator = new ApicurioRegistryBundleOperatorType();
-
+        ApicurioRegistryBundleOperatorType registryBundleOperator = new ApicurioRegistryBundleOperatorType(
+                "/home/jenkins/workspace/operator/apicurio-operator/install-examples/apicurio-registry-install-examples/install/install.yaml"
+        );
+        
         operatorManager.installOperator(registryBundleOperator);
     }
 }
