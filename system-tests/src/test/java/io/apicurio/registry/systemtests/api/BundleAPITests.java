aa8845f798484930c45d8e81ccba2cb8fa2e2ba7
Radim Kubis <rkubis@redhat.com>
2022-12-13 09:44:52 +0100

    Update(bundle): Do not use static path to install.yaml

--- system-tests/src/test/java/io/apicurio/registry/systemtests/api/BundleAPITests.java
+++ system-tests/src/test/java/io/apicurio/registry/systemtests/api/BundleAPITests.java
@@ -18,9 +18,7 @@ public class BundleAPITests extends APITests {
     public void testBeforeEach(ExtensionContext testContext) throws InterruptedException {
         LOGGER.info("BeforeEach: " + testContext.getTestMethod().get().getName());
 
-        ApicurioRegistryBundleOperatorType registryBundleOperator = new ApicurioRegistryBundleOperatorType(
-                "/home/jenkins/workspace/operator/apicurio-operator/install-examples/apicurio-registry-install-examples/install/install.yaml"
-        );
+        ApicurioRegistryBundleOperatorType registryBundleOperator = new ApicurioRegistryBundleOperatorType();
 
         operatorManager.installOperator(registryBundleOperator);
     }
